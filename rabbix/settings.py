a613cc197cd8597d167de72edb992d8ff419bb65
ramazon07-cmd <ustau2453@gmail.com>
2025-07-20 15:50:41 +0500

    ll

--- rabbix/settings.py
+++ rabbix/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
